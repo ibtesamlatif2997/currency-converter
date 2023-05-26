64a499e946df6c07b1383a1cf9b4c6535a863d99
Ibtesam Latif <ibtesam.ibee@gmail.com>
2023-05-26 19:31:21 +0400

    feat: popular currencies component

--- src/app/currency-converter/currency-converter.module.ts
+++ src/app/currency-converter/currency-converter.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
 import { ConverterRoutingModule } from './currency-converter-routing.module';
-
+import { PopularCurrenciesComponent } from './popular-currencies/popular-currencies.component';
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent,
+    PopularCurrenciesComponent
   ],
   imports: [
     CommonModule,
---
+++ src/app/currency-converter/popular-currencies/popular-currencies.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-popular-currencies',
+  templateUrl: './popular-currencies.component.html',
+  styleUrls: ['./popular-currencies.component.scss']
+})
+export class PopularCurrenciesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
